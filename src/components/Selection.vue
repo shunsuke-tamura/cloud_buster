3d9235f54d03e81723bacd407bfc6b00c2869221
shunsuke.tamura <wwwxx4869@gmail.com>
2022-02-06 03:01:08 +0900

    update: selectionsをコンポーネント化

---
+++ src/components/Selection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="selection-container">
+    <div id="lu">左上</div>
+    <div id="ru">右上</div>
+    <div id="ld">左下</div>
+    <div id="rd">右下</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Selection"
+}
+</script>
+
+<style>
+#selection-container {
+  display: grid;
+  grid-template-columns: 246px 246px;
+  grid-template-rows: 100px 100px;
+}
+#lu {
+  border: 3px solid;
+  grid-row: 1; grid-column: 1;
+  margin: 10px 2.5px 2.5px 10px;
+}
+#ru {
+  border: 3px solid;
+  grid-row: 1; grid-column: 2;
+  margin: 10px 10px 2.5px 2.5px;
+}
+#ld {
+  border: 3px solid;
+  grid-row: 2; grid-column: 1;
+  margin: 2.5px 2.5px 10px 10px;
+}
+#rd {
+  border: 3px solid;
+  grid-row: 2; grid-column: 2;
+  margin: 2.5px 10px 10px 2.5px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,22 +22,19 @@
         <div id="player-status"></div>
       </div>
       <div id="sys-msg"></div>
-      <div id="selection-container">
-        <div id="lu">左上</div>
-        <div id="ru">右上</div>
-        <div id="ld">左下</div>
-        <div id="rd">右下</div>
-      </div>
+      <selection />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios"
+import Selection from '../components/Selection.vue'
 export default {
   name: 'Home',
 
   components: {
+    Selection
   },
   data() {
     return {
@@ -129,32 +126,5 @@ export default {
   border: 3px solid;
   margin: 10px 10px auto 10px;
   height: 65px;
-}
-
-/*grid*/
-#selection-container {
-  display: grid;
-  grid-template-columns: 246px 246px;
-  grid-template-rows: 100px 100px;
-}
-#lu {
-  border: 3px solid;
-  grid-row: 1; grid-column: 1;
-  margin: 10px 2.5px 2.5px 10px;
-}
-#ru {
-  border: 3px solid;
-  grid-row: 1; grid-column: 2;
-  margin: 10px 10px 2.5px 2.5px;
-}
-#ld {
-  border: 3px solid;
-  grid-row: 2; grid-column: 1;
-  margin: 2.5px 2.5px 10px 10px;
-}
-#rd {
-  border: 3px solid;
-  grid-row: 2; grid-column: 2;
-  margin: 2.5px 10px 10px 2.5px;
 }
 </style>
